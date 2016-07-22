872b7d2463e240172d68c3cffd85867723bb3895
Nikolay Chetverikov <nikolay@chetverikov.pro>
2016-07-23 01:56:08 +0300

    Delete prints

--- ikura-san.py
+++ ikura-san.py
@@ -8,16 +8,13 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(content_types=["text"])
 def response_text(message):
-    print(message)
     if (message.text == 'сдохни') or (message.text == 'die'):
         bot.send_message(message.chat.id, '[хокку и сеппуку')
-        print('Aborted')
         raise RuntimeError('User aborted operation')
     if (message.text == 'икура'):
         bot.send_message(message.chat.id, 'чо?')
     else:
         bot.send_message(message.chat.id, message.text)
 
-print('Starting')
 if __name__ == '__main__':
      bot.polling(none_stop=True)
