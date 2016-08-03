65cad73199881e104738bbbd4c7d2cc0b56bcf9a
Nikolay Chetverikov <nikolay@chetverikov.pro>
2016-08-03 23:51:08 +0300

    d

--- ikurasan.py
+++ ikurasan.py
@@ -100,8 +100,7 @@ def command_text_ikura(m):
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
-    pass
-    #bot.reply_to(message, message.text)
+    bot.reply_to(message, message.text)
 
 @server.route("/bot", methods=['POST'])
 def getMessage():
