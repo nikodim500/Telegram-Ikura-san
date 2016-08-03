b56efa4b83aafbe9f64bf73bcd27cca86318ec2d
Nikolay Chetverikov <nikolay@chetverikov.pro>
2016-08-03 03:01:57 +0300

    ikura

--- ikurasan.py
+++ ikurasan.py
@@ -1,28 +1,5 @@
 # -*- coding: utf-8 -*-
 #
-# import telebot
-# #import os
-# #from flask import Flask
-#
-# #app = Flask(__name__)
-# token = '225635767:AAGEvQKd4Cj8wNN24wM5hpd8FlgiJPmky0A'
-#
-# bot = telebot.TeleBot(token)
-#
-# @bot.message_handler(content_types=["text"])
-# def response_text(message):
-#     if (message.text == 'сдохни') or (message.text == 'die'):
-#         bot.send_message(message.chat.id, '[хокку и сеппуку')
-#         raise RuntimeError('User aborted operation')
-#     if (message.text == 'икура'):
-#         bot.send_message(message.chat.id, 'чо?')
-#     else:
-#         bot.send_message(message.chat.id, message.text)
-#
-# if __name__ == '__main__':
-#     bot.polling(none_stop=True)
-# #    port = int(os.environ.get("PORT", 5000))
-# #    app.run(host='0.0.0.0', port=port)
 
 
 import telebot
@@ -36,6 +13,10 @@ server = Flask(__name__)
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+
+@bot.message_handler(func=lambda message: message.text == "икура")
+def command_text_ikura(m):
+    bot.send_message(m.chat.id, "Ну чево тебе?")
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
